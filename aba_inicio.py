19402f65996320a193c8c8cb3a9560ff9503656c
Lucas Melo <94499582+stereolovve@users.noreply.github.com>
2024-10-22 18:25:41 -0300

    Add more modules to aba_inicio.py

--- aba_inicio.py
+++ aba_inicio.py
@@ -1,5 +1,6 @@
 import flet as ft
-
+from database import Sessao
+from sqlalchemy.exc import SQLAlchemyError
 
 
 def setup_aba_inicio(self):
@@ -52,3 +53,42 @@ def remover_campo_movimento(self, movimento_input, remover_button):
             self.movimentos_container.controls.remove(row)
             break
     self.page.update()
+
+
+def validar_campos(self):
+    campos_obrigatorios = [
+        (self.pesquisador_input, "Pesquisador"),
+        (self.codigo_ponto_input, "Código"),
+        (self.nome_ponto_input, "Ponto"),
+        (self.horas_contagem_input, "Periodo"),
+        (self.data_ponto_input, "Data do Ponto")
+    ]
+
+    for campo, nome in campos_obrigatorios:
+        if not campo.value:
+            snackbar = ft.SnackBar(ft.Text(f"{nome} é obrigatório!"), bgcolor="ORANGE")
+            self.page.overlay.append(snackbar)
+            snackbar.open = True
+            self.page.update()
+            return False
+
+    if not self.movimentos_container.controls:
+        snackbar = ft.SnackBar(ft.Text("Adicione pelo menos um movimento!"), bgcolor="ORANGE")
+        self.page.overlay.append(snackbar)
+        snackbar.open = True
+        self.page.update()
+        return False
+
+    try:
+        sessao_existente = self.session.query(Sessao).filter_by(sessao=f"Sessao_{self.codigo_ponto_input.value}_{self.nome_ponto_input.value}_{self.data_ponto_input.value}").first()
+        if sessao_existente:
+            snackbar = ft.SnackBar(ft.Text("Sessão já existe com esses detalhes!"), bgcolor="YELLOW")
+            self.page.overlay.append(snackbar)
+            snackbar.open = True
+            self.page.update()
+            return False
+    except SQLAlchemyError as ex:
+        print(f"Erro ao validar campos: {ex}")
+        return False
+
+    return True
