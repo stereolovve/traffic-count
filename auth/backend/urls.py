4c6a2196f85c4f5364f61f006b7a6808a19d92f0
Lucas Melo <94499582+stereolovve@users.noreply.github.com>
2024-11-25 16:03:31 -0300

    Update urls.py

--- auth/backend/urls.py
+++ auth/backend/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Hello, world!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('autenticacao.urls')),
+    path('', home),
 ]
