c1f558c2bd25c6016963e948a38c6142200c9bd5
Lucas Melo <lucas.melo@perplan.local>
2025-04-06 18:03:28 -0300

    Fix settings.py

--- auth/backend/settings.py
+++ auth/backend/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = 'var/www/traffic-count/auth/template/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
